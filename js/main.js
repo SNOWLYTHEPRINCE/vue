b020bcb653e1cc6f38b41f9ce985bd8089ef1d3b
Сергей Смышляев <gr423_smsig@lab-407-7.tomtit-tomsk.ru>
2024-04-22 11:35:07 +0700

    Finished 4 task

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
  }) 
